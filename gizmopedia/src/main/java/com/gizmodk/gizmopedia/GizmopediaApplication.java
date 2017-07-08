a635212f70eb34ff669b8169ecf8e15b42c9428f
gizmoduck <avijitchark@gmail.com>
2017-07-09 00:02:30 +0530

    initial commit

---
+++ gizmopedia/src/main/java/com/gizmodk/gizmopedia/GizmopediaApplication.java
@@ -0,0 +1,12 @@
+package com.gizmodk.gizmopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GizmopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GizmopediaApplication.class, args);
+	}
+}
